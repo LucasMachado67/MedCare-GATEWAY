8ab8723503d785548dae0b3e855d7d9ba5419746
lucasmachado67 <lucasem80@gmail.com>
2025-12-19 08:20:33 -0300

    resolucao de bugs

--- src/main/java/com/ms/gateway/controller/FallBackController.java
+++ src/main/java/com/ms/gateway/controller/FallBackController.java
@@ -1,30 +1,29 @@
-package com.ms.gateway.controller;
+// package com.ms.gateway.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.web.bind.annotation.RestController;
 
-import reactor.core.publisher.Mono;
+// import reactor.core.publisher.Mono;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RequestMapping;
 
 
-@RestController
-@RequestMapping("fallback")
-public class FallBackController {
+// @RestController
+// @RequestMapping("fallback")
+// public class FallBackController {
 
-    @GetMapping("/auth")
-    public Mono<String> authFallback() {
-        return Mono.just("Serviço de autenticação está temporariamente indisponível.");
-    }
+//     @RequestMapping("/auth")
+//     public Mono<String> authFallback() {
+//         return Mono.just("Serviço de autenticação está temporariamente indisponível.");
+//     }
 
-    @GetMapping("/appointments")
-    public Mono<String> appointmentsFallback() {
-        return Mono.just("Serviço de agendamento está temporariamente indisponível.");
-    }
+//     @RequestMapping("/appointments")
+//     public Mono<String> appointmentsFallback() {
+//         return Mono.just("Serviço de agendamento está temporariamente indisponível.");
+//     }
 
-    @GetMapping("/entity")
-    public Mono<String> entityFallback() {
-        return Mono.just("Serviço de entidade está temporariamente indisponível.");
-    }
+//     @RequestMapping("/entity")
+//     public Mono<String> entityFallback() {
+//         return Mono.just("Serviço de entidade está temporariamente indisponível.");
+//     }
     
-}
+// }
